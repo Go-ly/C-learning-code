be27f773bb939803a1e1138449a70bde0b7e540e
Go-ly <2598932509@qq.com>
2021-06-14 15:21:33 +0800

    通讯录优化升级，实现动态内存管理

---
+++ 6-14-1通讯录优化升级，实现内存动态管理/通讯录优化升级，实现内存动态管理/test.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "function.h"
+
+int main()
+{
+	struct contact con;
+	
+	Ini(&con);        //写一个函数用来刚开始对通讯录进行初始化
+	int input = 0;
+	do
+	{
+		menu1();
+		printf("请选择所要进行的操作:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			Add(&con);
+			break;
+		case DEL:
+			Delete(&con);
+			break;
+		case SEARCH:
+			Search(&con);
+			break;
+		case MODIFY:
+			Modify(&con);
+			break;
+		case SHOW:
+			Show(&con);
+			break;
+		case SORT:
+			Sort(&con);
+			break;
+		case 0:
+			Destroy(&con);
+			printf("退出程序\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+}
